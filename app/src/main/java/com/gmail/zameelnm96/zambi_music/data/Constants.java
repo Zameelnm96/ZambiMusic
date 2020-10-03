2c4dcd52b5a0b26d46f2021089a0e27a35f9144b
Zameel <zameelnm96@gmail.com>
2020-10-04 02:41:37 +0530

    initial project created

---
+++ app/src/main/java/com/gmail/zameelnm96/zambi_music/data/Constants.java
@@ -0,0 +1,4 @@
+package com.gmail.zameelnm96.zambi_music.data;
+
+public abstract class Constants {
+}
---
+++ app/src/main/java/com/gmail/zameelnm96/zambi_music/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.gmail.zameelnm96.zambi_music.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gmail.zambi_music_mvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
